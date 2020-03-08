0765cd2ef91ccfb06c8d858f438f01e72a1daf15
Antonio Fernandes <antoniopaisfernandes@gmail.com>
2020-03-08 15:19:33 +0000

    Creat 'student' model

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Student;
+use Faker\Generator as Faker;
+
+$factory->define(Student::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'address' => $faker->address,
+        'postal_code' => $faker->postcode,
+        'city' => $faker->city,
+        'citizen_id' => $faker->randomNumber(8),
+        'email' => $faker->safeEmail,
+        'phone' => $faker->phoneNumber,
+        'birth_place' => $faker->city,
+        'nationality' => 'PORTUGAL',
+        'current_job_title' => $faker->jobTitle,
+    ];
+});
---
+++ database/migrations/2020_03_08_150127_create_students_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('postal_code', 20);
+            $table->string('city');
+            $table->string('citizen_id', 20)->nullable()->unique();
+            $table->date('citizen_id_validity')->nullable();
+            $table->string('email')->unique();
+            $table->string('phone', 20);
+            $table->string('birth_place')->nullable();
+            $table->string('nationality')->nullable();
+            $table->string('current_job_title')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}

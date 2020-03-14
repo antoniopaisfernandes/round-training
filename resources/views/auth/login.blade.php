d5737e4951e369e21efe6912a01cd87529f9735b
Antonio Fernandes <antoniopaisfernandes@gmail.com>
2020-03-14 11:30:29 +0000

    Refactor login template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+<div class="tw-mx-auto tw-w-full md:tw-w-1/2 tw-max-w-md">
+    <v-form
+        method="POST"
+        action="{{ route('login') }}"
+        @keyup.enter.native="submit"
+    >
+        @csrf
+        <v-card class="elevation-12">
+            <v-toolbar
+                color="primary"
+                dark
+                flat
+            >
+                <v-toolbar-title>Login</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+                <v-text-field
+                    id="email"
+                    label="{{ __('E-Mail Address') }}"
+                    name="email"
+                    prepend-icon="mdi-account-outline"
+                    type="text"
+                    :dense="true"
+                    hide-details="auto"
+                    required
+                    filled
+                ></v-text-field>
+                @error('email')
+                    <span class="tw-text-red-600" role="alert">
+                        {{ $message }}
+                    </span>
+                @enderror
+                <v-text-field
+                    id="password"
+                    label="{{ __('Password') }}"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                    :dense="true"
+                    hide-details="auto"
+                    required
+                    filled
+                ></v-text-field>
+                @error('password')
+                    <span class="tw-text-red-600" role="alert">
+                        {{ $message }}
+                    </span>
+                @enderror
+            </v-card-text>
+            <v-card-actions>
+                <div class="tw-flex tw-flex-col tw-w-full">
+                    <div class="tw-ml-auto">
+                        <v-checkbox
+                            class="tw-mt-0"
+                            name="remember"
+                            id="remember"
+                            {{ old('remember') ? 'checked' : '' }}
+                            label="{{ __('Remember Me') }}"
+                            hide-details="true"
+                        ></v-checkbox>
+                    </div>
+                    <v-btn
+                        class="tw-mt-6"
+                        color="primary"
+                        type="submit"
+                    >{{ __('Login') }}</v-btn>
+                    @if (Route::has('password.request'))
+                        <div class="tw-mt-6">
+                            <a href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    @endif
                 </div>
-            </div>
-        </div>
-    </div>
+            </v-card-actions>
+        </v-card>
+    </v-form>
 </div>
 @endsection

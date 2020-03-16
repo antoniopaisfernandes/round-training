7d842509b01d99a875a4459c2939f2bf42590f80
Antonio Fernandes <antoniopaisfernandes@gmail.com>
2020-03-16 17:41:02 +0000

    Update index.blade.php

--- resources/views/program/index.blade.php
+++ resources/views/program/index.blade.php
@@ -1,7 +1,11 @@
-<div class="container">
-    @foreach ($programs as $program)
-        {{ $program->name }}
-    @endforeach
-</div>
+@extends('layouts.app')
 
-{{ $programs->links() }}
+@section('content')
+    <div class="container">
+        @foreach ($programs as $program)
+            {{ $program->name }} <br />
+        @endforeach
+    </div>
+
+    {{ $programs->links() }}
+@endsection

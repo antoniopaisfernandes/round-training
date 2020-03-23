7a92150806d57d690ebbeb460250dede41696e76
Antonio Fernandes <antoniopaisfernandes@gmail.com>
2020-03-23 11:32:15 +0000

    Increase randomness with company names

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Company::class, function (Faker $faker) {
     return [
-        'name' => $faker->company,
+        'name' => $faker->company . $faker->randomNumber(2),
         'vat_number' => $faker->randomNumber(9, true),
     ];
 });

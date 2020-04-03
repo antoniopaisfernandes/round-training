76712318a7e084f509d59cf45bf2f59d134bb1fb
Antonio Fernandes <antoniopaisfernandes@gmail.com>
2020-04-03 12:00:50 +0100

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,4 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import { Model } from 'vue-api-query'
 
 require('./bootstrap')
 require('./plugins/event-bus')
@@ -10,11 +6,7 @@ require('./components/')
 
 import vuetify from './plugins/vuetify'
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Model.$http = window.axios
 
 const app = new Vue({
     vuetify,

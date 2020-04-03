30bdbcc614509809ee4c5e6032f50ef5ccd59231
Antonio Fernandes <antoniopaisfernandes@gmail.com>
2020-04-03 18:22:11 +0100

    WIP

--- resources/js/models/Model.js
+++ resources/js/models/Model.js
@@ -1,28 +1,41 @@
 import { Model as BaseModel } from 'vue-api-query'
 import cloneDeep from 'lodash-es/cloneDeep'
+import isObject from 'lodash-es/isObject'
+
+const getCircularReplacer = () => {
+  const set = new WeakSet()
+  return (key, value) => {
+    if (isObject(value) && value !== null) {
+      if (set.has(value)) {
+        return
+      }
+      set.add(value)
+    }
+    return value
+  }
+}
 
 export default class Model extends BaseModel {
 
-  // ---------------------------
-  // vue-api-query related
-  // ---------------------------
   baseURL() {
     return ''
   }
-  request(config) {
-    return this.$http.request(config)
-  }
 
-  // ---------------------------
-  // Other
-  // ---------------------------
-  className() {
-    return this.constructor.name
+  request(config) {
+    return this.$http.request({
+      // Fix circular reference bug in _builder prop
+      transformRequest: [(data) => {
+        return isObject(data)
+          ? JSON.parse(JSON.stringify(data, getCircularReplacer()))
+          : data
+      }, ...this.$http.defaults.transformRequest],
+      ...config
+    })
   }
 
   clone() {
     const obj = cloneDeep(this)
-
     return obj
   }
+
 }

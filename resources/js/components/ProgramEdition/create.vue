abab274aa7654ca6901d019840dfac1803af79b7
Antonio Fernandes <antoniopaisfernandes@gmail.com>
2020-04-04 15:38:52 +0100

    Add students tab to program edition dialog

--- resources/js/components/ProgramEdition/create.vue
+++ resources/js/components/ProgramEdition/create.vue
@@ -1,169 +1,185 @@
 <template>
-  <v-dialog persistent scrollable v-model="dataVisible" @keydown.esc="dataVisible = false" max-width="48rem">
-    <v-card :loading="isSaving" class="px-5 py-5" height="90vh">
-      <v-card-title>
-        <span class="headline">Curso</span>
-      </v-card-title>
-      <v-card-text class="mt-5">
-        <div class="tw-flex">
-          <div class="tw-flex tw-flex-row tw-justify-center tw-items-center tw-w-2/3">
+  <v-dialog persistent v-model="dataVisible" @keydown.esc="dataVisible = false" max-width="48rem">
+    <v-card :loading="isSaving" class="px-5 py-5 vert-card" height="90vh">
+      <v-tabs
+        fixed-tabs
+        v-model="tab"
+      >
+        <v-tab key="programEdition">Curso</v-tab>
+        <v-tab key="students">Alunos</v-tab>
+      </v-tabs>
 
-            <v-select
-              :items="programs"
-              v-model="dataProgramEdition.program_id"
-              label="Nome do curso"
-              required
-              :rules="rules.program_id"
-              @input="dataProgramEdition.program_id = $event"
-              class="tw-w-8/12"
-            ></v-select>
+      <v-tabs-items v-model="tab">
+        <v-tab-item key="programEdition">
+          <v-card>
+            <v-card-text>
+              <div class="tw-flex">
+                <div class="tw-flex tw-flex-row tw-justify-center tw-items-center tw-w-2/3">
 
-            <v-btn
-              fab
-              dark
-              x-small
-              color="primary"
-              @click="addProgramDialogVisible = true"
-              class="tw--mt-2"
-            >
-              <v-icon dark>mdi-plus</v-icon>
-            </v-btn>
-            <add-program-dialog
-              :existing-programs="programs"
-              :visible="addProgramDialogVisible"
-              @close="addProgramDialogVisible = false"
-              @input="selectProgramId($event)"
-            ></add-program-dialog>
+                  <v-select
+                    :items="programs"
+                    v-model="dataProgramEdition.program_id"
+                    label="Nome do curso"
+                    required
+                    :rules="rules.program_id"
+                    @input="dataProgramEdition.program_id = $event"
+                    class="tw-w-8/12"
+                  ></v-select>
 
-          </div>
-          <v-text-field
-            v-model="dataProgramEdition.name"
-            label="Nome da edição"
-            required
-            :rules="rules.name"
-            class="ml-4 tw-w-1/3"
-          ></v-text-field>
-        </div>
-        <div class="tw-flex tw-flex-row tw-justify-center tw-items-center">
-          <v-menu
-            v-model="startsAtActive"
-            :close-on-content-click="false"
-            :nudge-right="40"
-            transition="scale-transition"
-            offset-y
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on }">
-              <v-text-field
-                v-model="dataProgramEdition.starts_at"
-                label="Início a"
-                prepend-inner-icon="mdi-calendar"
-                readonly
-                v-on="on"
-                :rules="rules.starts_at"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="dataProgramEdition.starts_at"
-              @input="startsAtActive = false"
-            ></v-date-picker>
-          </v-menu>
-          <v-menu
-            v-model="endsAtActive"
-            :close-on-content-click="false"
-            :nudge-right="40"
-            transition="scale-transition"
-            offset-y
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on }">
-              <v-text-field
-                v-model="dataProgramEdition.ends_at"
-                label="Fim a"
-                prepend-inner-icon="mdi-calendar"
-                readonly
-                v-on="on"
-                class="tw-ml-2"
-                :rules="rules.ends_at"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="dataProgramEdition.ends_at"
-              @input="endsAtActive = false"
-            ></v-date-picker>
-          </v-menu>
-          <v-text-field
-            v-model="managerName"
-            label="Criado por"
-            :disabled="true"
-            class="ml-2"
-          ></v-text-field>
-        </div>
-        <div class="tw-flex tw-flex-row tw-justify-center tw-items-center">
-          <v-text-field
-            v-model="dataProgramEdition.supplier"
-            label="Fornecedor"
-            required
-            :rules="rules.supplier"
-          ></v-text-field>
-          <v-text-field
-            v-model="dataProgramEdition.teacher_name"
-            label="Formador"
-            required
-            :rules="rules.teacher_name"
-            class="ml-2"
-          ></v-text-field>
-          <v-text-field
-            v-model="dataProgramEdition.cost"
-            label="Valor"
-            required
-            :rules="rules.cost"
-            class="ml-2 tw-w-4 money"
-            prefix="€"
-          ></v-text-field>
-        </div>
+                  <v-btn
+                    fab
+                    dark
+                    x-small
+                    color="primary"
+                    @click="addProgramDialogVisible = true"
+                    class="tw--mt-2"
+                  >
+                    <v-icon dark>mdi-plus</v-icon>
+                  </v-btn>
+                  <add-program-dialog
+                    :existing-programs="programs"
+                    :visible="addProgramDialogVisible"
+                    @close="addProgramDialogVisible = false"
+                    @input="selectProgramId($event)"
+                  ></add-program-dialog>
 
-        <div class="mt-2">
-          <span class="subtitle-1">Agendamentos</span>
-          <v-btn
-            fab
-            dark
-            x-small
-            color="primary"
-            @click="addSchedule"
-            class="tw-ml-2 tw--mt-2"
-          >
-            <v-icon dark>mdi-plus</v-icon>
-          </v-btn>
-        </div>
+                </div>
+                <v-text-field
+                  v-model="dataProgramEdition.name"
+                  label="Nome da edição"
+                  required
+                  :rules="rules.name"
+                  class="ml-4 tw-w-1/3"
+                ></v-text-field>
+              </div>
+              <div class="tw-flex tw-flex-row tw-justify-center tw-items-center">
+                <v-menu
+                  v-model="startsAtActive"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      v-model="dataProgramEdition.starts_at"
+                      label="Início a"
+                      prepend-inner-icon="mdi-calendar"
+                      readonly
+                      v-on="on"
+                      :rules="rules.starts_at"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="dataProgramEdition.starts_at"
+                    @input="startsAtActive = false"
+                  ></v-date-picker>
+                </v-menu>
+                <v-menu
+                  v-model="endsAtActive"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      v-model="dataProgramEdition.ends_at"
+                      label="Fim a"
+                      prepend-inner-icon="mdi-calendar"
+                      readonly
+                      v-on="on"
+                      class="tw-ml-2"
+                      :rules="rules.ends_at"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="dataProgramEdition.ends_at"
+                    @input="endsAtActive = false"
+                  ></v-date-picker>
+                </v-menu>
+                <v-text-field
+                  v-model="managerName"
+                  label="Criado por"
+                  :disabled="true"
+                  class="ml-2"
+                ></v-text-field>
+              </div>
+              <div class="tw-flex tw-flex-row tw-justify-center tw-items-center">
+                <v-text-field
+                  v-model="dataProgramEdition.supplier"
+                  label="Fornecedor"
+                  required
+                  :rules="rules.supplier"
+                ></v-text-field>
+                <v-text-field
+                  v-model="dataProgramEdition.teacher_name"
+                  label="Formador"
+                  required
+                  :rules="rules.teacher_name"
+                  class="ml-2"
+                ></v-text-field>
+                <v-text-field
+                  v-model="dataProgramEdition.cost"
+                  label="Valor"
+                  required
+                  :rules="rules.cost"
+                  class="ml-2 tw-w-4 money"
+                  prefix="€"
+                ></v-text-field>
+              </div>
 
-        <div v-bind:key="i" v-for="(schedule, i) in dataProgramEdition.schedules" class="tw-flex">
-          <div class="tw-w-1/4">
-            <v-datetime-picker label="Início" v-model="schedule.starts_at" timeFormat="HH:mm"></v-datetime-picker>
-          </div>
-          <div class="tw-w-1/4 tw-ml-2">
-            <v-datetime-picker label="Fim" v-model="schedule.ends_at" timeFormat="HH:mm"></v-datetime-picker>
-          </div>
-          <div class="tw-w-1/4 tw-ml-2">
-            <v-datetime-picker label="Início intervalo" v-model="schedule.interval_start" timeFormat="HH:mm"></v-datetime-picker>
-          </div>
-          <div class="tw-w-20/100 tw-ml-2">
-            <v-text-field label="Duranção intervalo" v-model="schedule.interval_minutes"></v-text-field>
-          </div>
-          <div class="tw-w-5/100 tw-ml-2 tw-flex tw-items-center">
-            <v-btn
-              fab
-              dark
-              x-small
-              color="error"
-              @click="deleteSchedule(i)"
-              class="tw-ml-2 tw--mt-2"
-            >
-              <v-icon dark>mdi-minus</v-icon>
-            </v-btn>
-          </div>
-        </div>
-      </v-card-text>
+              <div class="mt-2">
+                <span class="subtitle-1">Agendamentos</span>
+                <v-btn
+                  fab
+                  dark
+                  x-small
+                  color="primary"
+                  @click="addSchedule"
+                  class="tw-ml-2 tw--mt-2"
+                >
+                  <v-icon dark>mdi-plus</v-icon>
+                </v-btn>
+              </div>
+
+              <div v-bind:key="i" v-for="(schedule, i) in dataProgramEdition.schedules" class="tw-flex">
+                <div class="tw-w-1/4">
+                  <v-datetime-picker label="Início" v-model="schedule.starts_at" timeFormat="HH:mm"></v-datetime-picker>
+                </div>
+                <div class="tw-w-1/4 tw-ml-2">
+                  <v-datetime-picker label="Fim" v-model="schedule.ends_at" timeFormat="HH:mm"></v-datetime-picker>
+                </div>
+                <div class="tw-w-1/4 tw-ml-2">
+                  <v-datetime-picker label="Início intervalo" v-model="schedule.interval_start" timeFormat="HH:mm"></v-datetime-picker>
+                </div>
+                <div class="tw-w-20/100 tw-ml-2">
+                  <v-text-field label="Duranção intervalo" v-model="schedule.interval_minutes"></v-text-field>
+                </div>
+                <div class="tw-w-5/100 tw-ml-2 tw-flex tw-items-center">
+                  <v-btn
+                    fab
+                    dark
+                    x-small
+                    color="error"
+                    @click="deleteSchedule(i)"
+                    class="tw-ml-2 tw--mt-2"
+                  >
+                    <v-icon dark>mdi-minus</v-icon>
+                  </v-btn>
+                </div>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item key="students">
+          <v-card>
+            <v-card-text>TODO</v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
 
       <v-card-actions>
         <v-spacer></v-spacer>
@@ -172,7 +188,6 @@
       </v-card-actions>
     </v-card>
   </v-dialog>
-
 </template>
 
 <script>
@@ -216,6 +231,8 @@ export default {
     startsAtActive: false,
     endsAtActive: false,
     addProgramDialogVisible: false,
+
+    tab: null,
   }),
 
   computed: {
@@ -319,5 +336,20 @@ export default {
 <style>
   .money input {
     text-align: end;
+  }
+
+  .vert-card {
+    display: flex !important;
+    flex-direction: column;
+  }
+
+  .v-tabs {
+    flex: none !important;
+  }
+
+  .v-tabs-items {
+    flex: 1;
+    position: relative;
+    overflow-y: auto;
   }
 </style>
--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -9,15 +9,3 @@ Vue.component('toast', toast)
 Vue.component('program-edition-index', programEditionIndex)
 Vue.component('company-index', companyIndex)
 Vue.component('student-index', studentIndex)
-
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))

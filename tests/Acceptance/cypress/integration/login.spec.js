b65b718057120dcee391395085802d6fc693da1e
Antonio Fernandes <antoniopaisfernandes@gmail.com>
2020-04-18 12:06:47 +0100

    Add/improve login tests

--- tests/Acceptance/cypress/integration/login.spec.js
+++ tests/Acceptance/cypress/integration/login.spec.js
@@ -1,16 +1,31 @@
 describe('Login', () => {
 
-    // it('shows the login page', () => {
-    //     cy.visit('/login').contains('Login')
-    // });
+    it('shows the login page', () => {
+        cy.visit('/login').contains('Login')
+    });
+
+    it('displays an error when using invalid credentials', () => {
+        cy.visit('/login');
+
+        cy.get('#email').type('error@esferasaude.pt');
+        cy.get('#password').type('wrong_password');
+        cy.get('button').contains('Login').click();
+
+        cy.contains('O seu utilizador ou password estão incorrectos.')
+    });
+
 
     it('can login using seed credentials', () => {
         cy.visit('/login');
 
         cy.get('#email').type('admin@esferasaude.pt');
         cy.get('#password').type('password');
-
         cy.get('button').contains('Login').click();
+
+        cy.contains('Cursos terminados');
+        cy.location().should((location) => {
+            expect(location.pathname).to.eq('/')
+        })
     });
 
 });

9ca26f010d48d47512c5a9de637bf490305af3fa
Antonio Fernandes <antoniopaisfernandes@gmail.com>
2020-05-23 19:17:50 +0100

    Add missing methods

--- app/Policies/EnrollmentPolicy.php
+++ app/Policies/EnrollmentPolicy.php
@@ -34,6 +34,29 @@ class EnrollmentPolicy
     }
 
     /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Enrollment  $enrollment
+     * @return mixed
+     */
+    public function update(User $user, Enrollment $enrollment)
+    {
+        return false;
+    }
+
+    /**
      * Determine whether the user can delete the model.
      *
      * @param  \App\User  $user

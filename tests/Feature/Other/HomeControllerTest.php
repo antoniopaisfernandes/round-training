fc00dbd5a29b2a87bd5c8f5fd3b968dc448ac942
Antonio Fernandes <antoniopaisfernandes@gmail.com>
2020-05-24 22:03:54 +0100

    Add 2 more tests

---
+++ tests/Feature/Other/HomeControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature\Other;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_shows_the_home_dashboard()
+    {
+        $response = $this->actingAs($this->createAdminUser())->get('/');
+
+        $response->assertOk();
+        $response->assertSee('Cursos terminados');
+        $response->assertSee('Cursos a decorrer');
+        $response->assertSee('Cursos futuros');
+    }
+}
